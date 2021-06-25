5f64cf9dbf6560b506b65a8a7633c0b1af8c8920
Marc Schleeweiß <mschleeweiss@gmail.com>
2021-06-25 22:32:08 +0200

    prepare hex grid in frontend

--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -3,15 +3,30 @@
     <NotFound v-if="notFound" />
     <RoomFull v-if="roomFull" />
     <div v-if="!notFound">
-    <h1>Lobby</h1>
-    <p>{{game}}</p>
+      <h1>Lobby</h1>
+      <p>{{ game }}</p>
+      <svg viewBox="0 0 1000 1000">
+        <defs>
+          <g id="pod">
+            <polygon
+              stroke="#000000"
+              stroke-width="0.5"
+              :points="hexPoints"
+            />
+          </g>
+        </defs>
+        <g class="pod-wrap">
+          <use xlink:href="#pod" transform="translate(0, 0)" />
+        </g>
+      </svg>
     </div>
   </div>
 </template>
 
 <script>
-import NotFound from '@/components/NotFound.vue'
-import RoomFull from '@/components/RoomFull.vue'
+import NotFound from '@/components/NotFound.vue';
+import RoomFull from '@/components/RoomFull.vue';
+import { Hex, Layout, Point } from '@/hex';
 
 export default {
   name: 'Room',
@@ -23,12 +38,21 @@ export default {
     return {
       game: null,
       notFound: false,
-      roomFull: false
-    }
+      roomFull: false,
+    };
   },
   computed: {
     gameId() {
       return this.$route.params.gameid;
+    },
+    hexPoints() {
+      return this.layout
+        .polygonCorners(new Hex(0, 0, 0))
+        .map((o) => `${Math.round(o.x)},${Math.round(o.y)}`)
+        .join(' ');
+    },
+    layout() {
+      return new Layout(Layout.flat, new Point(100, 100), new Point(0, 0));
     },
     socketId() {
       return this.$store.state.socketId;
@@ -41,12 +65,11 @@ export default {
   },
   sockets: {
     gameStateChanged(game) {
-      console.log(game);
       this.game = game;
     },
     exception(data) {
       this.notFound = data.message === 'game_not_found';
-      this.roomFull = data.message === 'room_full'
+      this.roomFull = data.message === 'room_full';
     },
   },
 };
