a272a3e67e8fbca51299751dd14f8f9630a690b5
Marc Schleeweiß <mschleeweiss@gmail.com>
2021-06-27 23:05:19 +0200

    render hex map

--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -2,21 +2,30 @@
   <div class="hc-room">
     <NotFound v-if="notFound" />
     <RoomFull v-if="roomFull" />
-    <div v-if="!notFound">
+    <div v-if="!notFound && !roomFull">
       <h1>Lobby</h1>
-      <p>{{ game }}</p>
-      <svg viewBox="0 0 1000 1000">
+      <svg viewBox="-1000 -1000 2000 2000">
         <defs>
           <g id="pod">
-            <polygon
-              stroke="#000000"
-              stroke-width="0.5"
-              :points="hexPoints"
-            />
+            <polygon :points="hexPoints" />
           </g>
         </defs>
-        <g class="pod-wrap">
-          <use xlink:href="#pod" transform="translate(0, 0)" />
+        <g class="hc-cell">
+          <use
+            :class="{
+              active: cell[1] === 0,
+              red:    cell[1] === 1,
+              green:  cell[1] === 2,
+              yellow: cell[1] === 3,
+              purple: cell[1] === 4,
+              orange: cell[1] === 5,
+              blue:   cell[1] === 6,
+              }"
+            v-for="cell in map"
+            :key="cell"
+            xlink:href="#pod"
+            :transform="calcTransformation(cell[0])"
+          />
         </g>
       </svg>
     </div>
@@ -45,6 +54,9 @@ export default {
     gameId() {
       return this.$route.params.gameid;
     },
+    map() {
+      return this.game?.board.map ?? [];
+    },
     hexPoints() {
       return this.layout
         .polygonCorners(new Hex(0, 0, 0))
@@ -52,7 +64,7 @@ export default {
         .join(' ');
     },
     layout() {
-      return new Layout(Layout.flat, new Point(100, 100), new Point(0, 0));
+      return new Layout(Layout.pointy, new Point(100, 100), new Point(0, 0));
     },
     socketId() {
       return this.$store.state.socketId;
@@ -62,6 +74,13 @@ export default {
     this.$socket.client.emit('joinGame', {
       gameId: this.gameId,
     });
+  },
+  methods: {
+    calcTransformation(coords) {
+      const hex = new Hex(coords.q, coords.r, coords.s);
+      const point = this.layout.hexToPixel(hex);
+      return `translate(${point.x},${point.y})`;
+    },
   },
   sockets: {
     gameStateChanged(game) {
@@ -78,5 +97,36 @@ export default {
 <style lang="scss" scoped>
 .hc-room {
   height: 100%;
+}
+
+.hc-cell use {
+  fill: $current-line;
+  stroke: $comment;
+  stroke-width: 0.5rem;
+  transition: all 0.5s ease;
+}
+
+.hc-cell use.active:hover {
+  cursor: pointer;
+  fill: $foreground;
+}
+
+.hc-cell use.red {
+  fill: $red;
+}
+.hc-cell use.green {
+  fill: $green;
+}
+.hc-cell use.yellow {
+  fill: $yellow;
+}
+.hc-cell use.purple {
+  fill: $purple;
+}
+.hc-cell use.orange {
+  fill: $orange;
+}
+.hc-cell use.blue {
+  fill: $cyan;
 }
 </style>
