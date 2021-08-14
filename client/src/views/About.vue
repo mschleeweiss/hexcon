1bde6b0c1cbb9582d740bd16f4ab67c634e92fab
Marc Schleeweiß <mschleeweiss@gmail.com>
2021-08-14 17:17:30 +0200

    prepare legal notice

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,30 +1,52 @@
 <template>
   <div class="hc-about">
     <h1 class="hc-nova">About</h1>
+    <h4 class="hc-nova">Legal notice</h4>
+    <p>Information according to § 5 TMG</p>
+    <ul>
+      <li>Max Mustermann</li>
+      <li>Sample street 111</li>
+      <li>Building 44</li>
+      <li>90210 sample city</li>
+    </ul>
+
+    <h4 class="hc-nova">Contact</h4>
+    <ul>
+      <li>Phone: +49 (0) 123 44 55 66</li>
+      <li>E-mail: mustermann@musterfirma.de</li>
+    </ul>
     <h4 class="hc-nova">Data/Privacy</h4>
-    <p>Yes, I store information in your browser. But these are not cookies to track
-    you. I simply store your player name for when you visit this page again. And
-    also a session ID for when you loose connection or refresh your browser tab.
-    And I will keep it that way, so there is no need for that ugly cookie
-    consent banner.</p>
+    <p>
+      Yes, I store information in your browser. But these are not cookies to
+      track you. I simply store your player name for when you visit this page
+      again. And also a session ID for when you loose connection or refresh your
+      browser tab. And I will keep it that way, so there is no need for that
+      ugly cookie consent banner.
+    </p>
     <h4 class="hc-nova">Techstack</h4>
     <ul>
       <li><a href="https://nestjs.com/">NestJS</a></li>
       <li><a href="https://v3.vuejs.org/">Vue.js 3</a></li>
       <li><a href="https://socket.io/">socket.io</a></li>
-      <li><a href="https://github.com/catdad/canvas-confetti">canvas-confetti</a></li>
+      <li>
+        <a href="https://github.com/catdad/canvas-confetti">canvas-confetti</a>
+      </li>
       <li><a href="https://fontawesome.com/">Font Awesome</a></li>
-      </ul>
+    </ul>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .hc-about {
-  padding: 3rem;
+  padding: 2rem 4rem;
   height: 100%;
 
-  & h1, & h4, & p {
-    margin-bottom: 1rem;
+  & h4 {
+    margin-top: 2rem;
+  }
+  & p,
+  & ul {
+    margin-top: 1rem;
   }
 
   & ul {
--- client/src/views/HowTo.vue
+++ client/src/views/HowTo.vue
@@ -7,6 +7,11 @@
       You can place your tiles on any free cell on the board. However it has to
       have a neighboring cell which is not empty.
     </p>
+    <p>
+      <strong>Exception:</strong> During the first round every player has to
+      place its tiles in one of the corners. Every player has to chose their own
+      corner.
+    </p>
 
     <h4 class="hc-nova">How is the score calculated?</h4>
     <img src="@/assets/single_tile.jpg" />
@@ -19,26 +24,40 @@
       one or more extra turns then you will receive your new tiles after the
       final extra turn.
     </p>
-    <p><strong>Exception:</strong></p>
-    If you have no tiles with the color of your lowest scoring color you have
-    the possibility to return all of your current tiles and draw six new ones.
+    <p>
+      <strong>Exception:</strong> If you have no tiles with the color of your
+      lowest scoring color you have the possibility to return all of your
+      current tiles and draw six new ones.
+    </p>
     <img src="@/assets/swap.jpg" />
     <p>
-      The lowest scoring color for "Player" is yellow. "Player" has no yellow
+      The lowest scoring color for <em>Player</em> is yellow. <em>Player</em> has no yellow
       tiles at the end of his turn. They can now either replace
       <strong>all of their tiles</strong> or simply resume and draw one tile (as
-      usual)
+      usual).
     </p>
     <h4 class="hc-nova">How do I win?</h4>
     <p>The highest lowest score wins. But what does that mean?</p>
+    <p>
+      It doesn't matter how many times you maxed out a color. If your lowest
+      score is lower than your opponents lowest score then you will lose the
+      game. So the scores are compared from the lowest to the highest.
+    </p>
     <img src="@/assets/winner.jpg" />
-    <p>The lowest score for both players is 9</p>
+    <p>
+      The lowest score of <em>Player</em> is 9 (yellow). The lowest score of
+      <em>Enemy</em> is also 9 (yellow). Therefore the second lowest scores have
+      to be compared: <em>Player</em>'s second lowest score is a 11 (purple),
+      <em>Enemie</em>'s second lowest score is 10 (purple). This means that
+      <em>Player</em> wins this game. They had the
+      <em>highest lowest score</em>.
+    </p>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .hc-howto {
-  padding: 3rem;
+  padding: 2rem 4rem;
   height: 100%;
   overflow: auto;
 
